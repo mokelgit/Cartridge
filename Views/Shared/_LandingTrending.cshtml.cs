6200f139ed2d376dd7799689b9913ea03687b564
mokelgit <maym2018@gmail.com>
2026-05-28 16:07:39 -0400

    Progress on landing page and css structure.

---
+++ Views/Shared/_LandingTrending.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cartridge.Views.Shared
+{
+    public class _LandingTrendingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Shared/_Logo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cartridge.Views.Shared
+{
+    public class logoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
